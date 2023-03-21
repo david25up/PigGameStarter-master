01781cd5f1211844e5d1e49884b98eb0d0c786f4
Ash <ashtonchar22@gmail.com>
2023-03-21 16:29:46 -0700

    Set up ctor's and privatized variables

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -2,11 +2,21 @@ package edu.up.cs301.pig;
 
 public class PigGameState extends GameState {
 
-    int whoseTurn;
-    int player0Score;
-    int player1Score;
-    int addedTotal;
-    int dieValue;
+    private int whoseTurn;
+    private int player0Score;
+    private int player1Score;
+    private int addedTotal;
+    private int dieValue;
 
 
+    /** Default constructor for the game state */
+    public PigGameState() {
+    }//default ctor
+
+    /**
+     * Copy constructor of the default game state
+     * @param original   The game state object we are copying over
+     */
+    public PigGameState(PigGameState original) {
+    }//ctor
 }
