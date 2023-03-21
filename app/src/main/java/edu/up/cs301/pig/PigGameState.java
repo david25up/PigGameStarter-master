4d7f47168a72c29e54cfe772d5df0c8fa9551991
Matt <mddavid1302@gmail.com>
2023-03-21 16:30:24 -0700

    initialized variables to private

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -1,12 +1,15 @@
 package edu.up.cs301.pig;
 
+import edu.up.cs301.game.infoMsg.GameState;
+
 public class PigGameState extends GameState {
 
-    int whoseTurn;
-    int player0Score;
-    int player1Score;
-    int addedTotal;
-    int dieValue;
+    private int whoseTurn;
+    private int player0Score;
+    private int player1Score;
+    private int addedTotal;
+    private int dieValue;
 
 
+    public int
 }
