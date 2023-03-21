8a721d191313562abc2ac1b3915aa94cc1467303
Matt <mddavid1302@gmail.com>
2023-03-21 16:27:23 -0700

    initialized variables

---
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -0,0 +1,12 @@
+package edu.up.cs301.pig;
+
+public class PigGameState extends GameState {
+
+    int whoseTurn;
+    int player0Score;
+    int player1Score;
+    int addedTotal;
+    int dieValue;
+
+
+}
